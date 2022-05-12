77ea30bcc9e75ac7007b544fec67d0bc50389f67
Prasanth-Sai-Boyina <54104073+Prasanth-Sai-Boyina@users.noreply.github.com>
2022-05-12 10:02:43 +0530

    Add files via upload

---
+++ training/ae.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.layers import Dense
+from keras.layers import Dropout
+
+original_dim = 19
+intermediate_dim = 64
+latent_dim = 19
+
+
+ae_input = keras.Input(shape=(original_dim,), name="input")
+e_features=Dense(intermediate_dim, activation='relu')(ae_input)
+e_features=Dense(latent_dim, activation='relu')(e_features)
+d_features=Dense(intermediate_dim, activation='relu')(e_features)
+d_pred=Dense(original_dim, activation='sigmoid' ,name='same')(d_features)
+encoder=keras.Model(inputs=[ae_input],
+                    outputs=[e_features]
+                    )
+clf_features=Dense(128, activation='relu')(e_features)
+clf_features=Dropout(0.45)(clf_features)
+clf_features=Dense(64, activation='relu')(clf_features)
+clf_features=Dense(32, activation='relu')(clf_features)
+clf_features=Dense(16, activation='relu')(clf_features)
+clf_features=Dense(8, activation='relu')(clf_features)
+clf_features=Dense(4, activation='relu')(clf_features)
+clf_features=Dense(2, activation='relu')(clf_features)
+clf_pred=Dense(1, activation='sigmoid', name='stress')(clf_features)
+ae = keras.Model(
+    inputs=[ae_input],
+    outputs=[d_pred, clf_pred],
+)
+ae.compile(
+    optimizer='adam',
+    loss={
+        "same":keras.losses.MeanSquaredError(),
+        "stress":keras.losses.BinaryCrossentropy(),
+    },
+    loss_weights={"same":1, "stress":0.95},
+)
